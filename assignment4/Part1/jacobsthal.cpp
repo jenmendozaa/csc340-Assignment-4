4007f0b0e74cc6dca16c46e709a6bf3fcf754994
Nicocarrill0 <128573090+Nicocarrill0@users.noreply.github.com>
2024-11-12 22:51:44 -0800

    Added cmake folder && Wrapped up Part 1

--- assignment4/Part1/jacobsthal.cpp
+++ assignment4/Part1/jacobsthal.cpp
@@ -5,20 +5,25 @@
 // TO DO: implement jacobsthal
 //      Hint: it should return a long long (integers)
 
+long long jacobsthal(int n ) {
+        if (n < 2) return n;
+        return jacobsthal(n - 1) + 2 * jacobsthal(n - 2);
+    }
+
 
 int main(int argc, char* argv[]) {
     // Read n from command line argument
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " <n>" << std::endl;
-        return 1; 
+        return 1;
     }
 
     // Convert n to an integer
-    int n = std::atoi(argv[1]); 
+    int n = std::atoi(argv[1]);
 
     // Compute Jacobsthal of n
-    long long result = jacobsthal(n); 
+    long long result = jacobsthal(n);
     std::cout << "Jacobsthal(" << n << ") = " << result << std::endl;
 
-    return 0; 
+    return 0;
 }
